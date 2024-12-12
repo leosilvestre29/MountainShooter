a753d8c64007d22bdd840296c63cadce4fe2ef47
Leonardo Silvestre <leonardo.silvestre191@gmail.com>
2024-12-12 18:51:16 -0300

    ...

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, WIN_WIDTH, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
